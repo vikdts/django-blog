117e8cd7003ec6e26da1a9ba86932a69026becfa
vikdts <>
2023-11-17 12:31:12 +0000

    Edit allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-blog-app-e042810ea558.herokuapp.com/',
-                 'localhost']
+ALLOWED_HOSTS = ['codestar-blog-app-e042810ea558.herokuapp.com',
+                 'localhost', '8000-vikdts-django-blog-fh5v4aqmto.us2.codeanyapp.com']
 
 
 # Application definition
